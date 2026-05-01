6563fc61c1ec25fd21d7c1e4d239bf357d7840ee
Juan Martinez <juan.martinez@dynamics.upc.edu>
2026-05-01 13:20:11 +0200

    feat(mcu): estructuracion de launch_ctrl

---
+++ MCU/MCU_FW/lib/control_algorithms/launch_ctrl.c
@@ -0,0 +1,10 @@
+#include "launch_ctrl.h"
+
+void launch_ctrl_init(void) {
+    // Initialization of launch control states
+}
+
+void launch_ctrl_update(void) {
+    // Basic structure for launch control update
+    // Will be populated in the next commits
+}
---
+++ MCU/MCU_FW/lib/control_algorithms/launch_ctrl.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void launch_ctrl_init(void);
+void launch_ctrl_update(void);
+
+#ifdef __cplusplus
+}
+#endif
