e431e5eefa2a675932169a99bd73d72f877b103f
Aina Bonjoch <aina.bonjoch@dynamics.upc.edu>
2026-06-06 02:40:55 +0200

    feat(pdm): preparar CMakeLists.txt y configuraciones

---
+++ PDM/PDM_FW/include/pdm_config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// --- Hardware Pins ---
+#define MOSFET_PIN_LATCH    18
+#define MOSFET_PIN_ENABLE   34
+#define MUX_PIN_S0          25
+#define MUX_PIN_S1          26
+#define MUX_PIN_S2          27
+#define MUX_PIN_SIG         32
+
+// --- System Constants ---
+#define MUX_CHANNELS                  8
+#define MOCK_VBAT_MV                  12500
+#define PRECHARGE_HV_THRESHOLD_V      400
+#define PRECHARGE_TIMEOUT_MS          3000
--- PDM/PDM_FW/src/app.cpp
+++ PDM/PDM_FW/src/app.cpp
@@ -6,20 +6,7 @@
 #include "protection.h"
 #include "ipc.h"
 #include "can_service.h"
-
-// --- Hardware Pins ---
-#define MOSFET_PIN_LATCH    18
-#define MOSFET_PIN_ENABLE   34
-#define MUX_PIN_S0          25
-#define MUX_PIN_S1          26
-#define MUX_PIN_S2          27
-#define MUX_PIN_SIG         32
-
-// --- System Constants ---
-#define MUX_CHANNELS                  8
-#define MOCK_VBAT_MV                  12500
-#define PRECHARGE_HV_THRESHOLD_V      400
-#define PRECHARGE_TIMEOUT_MS          3000
+#include "pdm_config.h"
 
 static inline bool check_precharge_timeout(uint32_t start_ms) {
     return ((xTaskGetTickCount() * portTICK_PERIOD_MS) - start_ms) > PRECHARGE_TIMEOUT_MS;
